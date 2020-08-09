736a1e07c3e6bffc06bce3dd761b2e86ee530c90
Ritesh Gadekar <sonugadekar007@gmail.com>
2020-08-09 12:07:33 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,10 @@
 import json
 data=json.load(open("data.json"))
 def translate(w):
-   return data[w]
+   w=w.lower()
+   if w in data:
+      return data[w]
+   else:
+      print("Word dosn't exist in my dictionary, Please chexk it!")
 word = input("enter word:")
 print(translate(word))
